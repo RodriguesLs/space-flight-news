56326dfd6340aba0121325a02001a37933d3e8a6
Rodrigues <tercmt-feliperc@ftd.com.br>
2021-12-03 06:19:47 -0300

    feat(test): fix column name of article and create test to cover it.

--- space_flight_news/db/schema.rb
+++ space_flight_news/db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_023558) do
+ActiveRecord::Schema.define(version: 2021_12_03_024859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.boolean "featured"
+    t.string "title"
+    t.string "url"
+    t.string "image_url"
+    t.string "news_site"
+    t.string "summary"
+    t.string "published_at"
+    t.bigint "launch_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_articles_on_event_id"
+    t.index ["launch_id"], name: "index_articles_on_launch_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "provider"
@@ -27,4 +43,6 @@ ActiveRecord::Schema.define(version: 2021_12_03_023558) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "events"
+  add_foreign_key "articles", "launches"
 end
--- space_flight_news/test/models/article_test.rb
+++ space_flight_news/test/models/article_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save an article" do
+    launch = Launch.create(provider: 'some_launcher')
+    event = Event.create(provider: 'some_event')
+
+    article = Article.new(
+      featured: true,
+      title: 'some_title',
+      url: 'some_url',
+      image_url: 'some_img_url',
+      news_site: 'some_news_site',
+      summary: 'some_summary',
+      published_at: 'some_published_at',
+      launch: launch,
+      event: event
+    )
+
+    assert article.save
+  end
 end
