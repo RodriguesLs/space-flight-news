b7b83b4fb00e5c7fbb19bc2622ff5d814433e372
Rodrigues <tercmt-feliperc@ftd.com.br>
2021-12-05 14:21:15 -0300

    feat(serializer): include ams and create a serializer
    	Included gem active_model_serializer.
    	Created a article_serializer to provide a specific json object.

---
+++ app/serializers/article_serializer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ArticleSerializer < ActiveModel::Serializer
+  attributes :featured, :title, :url, :image_url, :news_site,
+             :summary, :published_at, :updated_at, :launches, :events
+
+  has_many :events, only: :id
+
+  def events
+    object.events.map { |e| { id: e.external_id, provider: e.provider } }
+  end
+
+  def launches
+    object.launches.map { |l| { id: l.external_id, provider: l.provider } }
+  end
+end
